6cd57f8abd32426eef1a14f1ef3a96446394aa09
RUCTHO <v.toropov@gmx.de>
2025-01-25 19:23:08 +0100

    adding stuff

--- src/main/java/com/rouec/crackonium/block/ModBlocks.java
+++ src/main/java/com/rouec/crackonium/block/ModBlocks.java
@@ -4,7 +4,9 @@ import com.rouec.crackonium.Crackonium;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
+import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,13 +14,21 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 
 public class ModBlocks {
-public static final Block LITHIUM_BLOCK = registerBlock("lithium_block",
-        new Block(AbstractBlock.Settings.create().mapColor(MapColor.DEEPSLATE_GRAY).requiresTool()
-                .strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)));
-
+    public static final Block LITHIUM_BLOCK = registerBlock("lithium_block",
+            new Block(AbstractBlock.Settings.create().mapColor(MapColor.DEEPSLATE_GRAY).requiresTool()
+                    .strength(5.0F, 6.0F).sounds(BlockSoundGroup.METAL)));
+    public static final Block LITHIUM_ORE = registerBlock("lithium_ore",
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).requiresTool().strength(3.0F, 3.0F)));
+    public static final Block DEEPSLATE_LITHIUM_ORE = registerBlock("deepslate_lithium_ore",
+            new ExperienceDroppingBlock(
+                    UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().mapColor(MapColor.DEEPSLATE_GRAY).sounds(BlockSoundGroup.DEEPSLATE).requiresTool().strength(4.0F, 4.0F)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -35,6 +45,8 @@ public static final Block LITHIUM_BLOCK = registerBlock("lithium_block",
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(ModBlocks.LITHIUM_BLOCK);
+            entries.add(ModBlocks.LITHIUM_ORE);
+            entries.add(ModBlocks.DEEPSLATE_LITHIUM_ORE);
         });
     }
 }
